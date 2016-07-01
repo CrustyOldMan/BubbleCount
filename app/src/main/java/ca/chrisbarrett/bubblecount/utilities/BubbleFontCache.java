6cb7050a42e49749edbb97714b1f9e4d3932dd0c
Chris Barrett <chris@chris-barrett.ca>
2016-07-01 09:08:29 -0700

    Refactored into statics in a separate package. Remove "magic" String references. Added documentation.

---
+++ app/src/main/java/ca/chrisbarrett/bubblecount/utilities/BubbleFontCache.java
@@ -0,0 +1,32 @@
+package ca.chrisbarrett.bubblecount.utilities;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+/**
+ * Utility class that initializes the Bubble & Soap Typeface statically. This is faster than initializes
+ * the Typeface on every call.
+ *
+ * @author Chris Barrett
+ * @see android.graphics.Typeface;
+ * @since June 30, 2016
+ */
+public class BubbleFontCache {
+
+    public static final String FONT_PATH = "fonts/bubble & soap.ttf";
+    private static Typeface typeface;
+
+    /**
+     * If the "Bubble & Soap" TrueTypeFont is not already cached, the Typeface will be loaded from
+     * {@value BubbleFontCache#FONT_PATH}.
+     *
+     * @param context used for calling {@link Context#getAssets()}
+     * @return the "Bubble & Soap" TrueTypeFont
+     */
+    public static Typeface getFont(Context context) {
+        if (typeface == null) {
+            typeface = Typeface.createFromAsset(context.getAssets(), FONT_PATH);
+        }
+        return typeface;
+    }
+}
---
+++ app/src/main/java/ca/chrisbarrett/bubblecount/utilities/PaintCache.java
@@ -0,0 +1,56 @@
+package ca.chrisbarrett.bubblecount.utilities;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+
+/**
+ * Utility class that initializes the Paint objects statically. This is faster than initializes
+ * the Paints on every call. Paints operate as the brush when drawing on a Canvas object
+ *
+ * @author Chris Barrett
+ * @see android.graphics.Paint
+ * @since Jul 01, 2016
+ */
+public class PaintCache {
+
+    public static final int DEFAULT_COLOR = Color.WHITE;
+    public static final int DEFAULT_LINE_THICKNESS = 2;
+    public static final int DEFAULT_TEXT_SIZE = 120;
+
+    private static Paint textPaint;
+    private static Paint drawPaint;
+
+    /**
+     * Generates the preset configuration for the text painter. The Paint object is assigned the
+     * {@link PaintCache#DEFAULT_COLOR} and {@link PaintCache#DEFAULT_TEXT_SIZE}. Text is also
+     * horizontally centered to paint.
+     *
+     * @return the Paint object used for text
+     */
+    public static Paint getTextPainter() {
+        if (textPaint == null) {
+            textPaint = new Paint();
+            textPaint.setColor(DEFAULT_COLOR);
+            textPaint.setTextSize(DEFAULT_TEXT_SIZE);
+            textPaint.setTextAlign(Paint.Align.CENTER);
+            textPaint.setStyle(Paint.Style.FILL);
+        }
+        return textPaint;
+    }
+
+    /**
+     * Generates the preset configuration for the drawable painter. The Paint object is assigned the
+     * {@link PaintCache#DEFAULT_COLOR} and {@link PaintCache#DEFAULT_LINE_THICKNESS}.
+     *
+     * @return the Paint object used for Drawables
+     */
+    public static Paint getDrawablePainter() {
+        if (drawPaint == null) {
+            drawPaint = new Paint();
+            drawPaint.setColor(DEFAULT_COLOR);
+            drawPaint.setStyle(Paint.Style.STROKE);
+            drawPaint.setStrokeWidth(DEFAULT_LINE_THICKNESS);
+        }
+        return drawPaint;
+    }
+}
