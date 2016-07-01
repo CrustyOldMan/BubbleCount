26efeedab27581976efb8cdf1223367640b57015
Chris Barrett <chris@chris-barrett.ca>
2016-07-01 09:19:29 -0700

    Added text support. Implemented collision and touch detection. Added documentation.

--- app/src/main/java/ca/chrisbarrett/bubblecount/model/BubbleSprite.java
+++ app/src/main/java/ca/chrisbarrett/bubblecount/model/BubbleSprite.java
@@ -1,56 +1,129 @@
 package ca.chrisbarrett.bubblecount.model;
 
 /**
- * Created by chrisbarrett on 2016-06-29.
+ * Concrete implementation of the Sprite for drawing Bubbles on the screen.
+ *
+ * @author Chris Barrett
+ * @see ca.chrisbarrett.bubblecount.model.Sprite
+ * @since Jun 29, 2016
  */
-public class BubbleSprite implements Sprite{
+public class BubbleSprite implements Sprite {
 
     private boolean isVisible;
     private float x;
     private float y;
     private float radius;
+    private String text;
 
+    /**
+     * Default constructor
+     *
+     * @param x      the X coordinate center of the BubbleSprite
+     * @param y      the Y coordinate center of the BubbleSprite
+     * @param radius the radius of the BubbleSprite
+     */
     public BubbleSprite(float x, float y, float radius) {
+        this(x, y, radius, null);
+    }
+
+    /**
+     * Default constructor
+     *
+     * @param x      the X coordinate center of the BubbleSprite
+     * @param y      the Y coordinate center of the BubbleSprite
+     * @param radius the radius of the BubbleSprite
+     * @param text   the text that is associated with the the BubbleSprite
+     */
+    public BubbleSprite(float x, float y, float radius, String text) {
         this.x = x;
         this.y = y;
         this.radius = radius;
         isVisible = true;
+        this.text = text;
     }
 
+    @Override
     public float getX() {
         return x;
     }
 
+    @Override
     public void setX(float x) {
         this.x = x;
     }
 
+    @Override
     public float getY() {
         return y;
     }
 
+    @Override
     public void setY(float y) {
         this.y = y;
     }
 
+    @Override
     public float getRadius() {
         return radius;
     }
 
+    @Override
     public void setRadius(float radius) {
         this.radius = radius;
     }
 
+    @Override
     public boolean isVisible() {
         return isVisible;
     }
 
+    @Override
     public void setVisible(boolean isVisible) {
         this.isVisible = isVisible;
+
+    }
+
+    @Override
+    public String getText() {
+        if (text == null) {
+            return "";
+        } else {
+            return text;
+        }
+    }
+
+    @Override
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean isTouched(float x, float y) {
+        return isOverlap(x, y, radius);
+    }
+
+    @Override
+    public boolean isCollision(Sprite sprite) {
+        return sprite != null && isOverlap(sprite.getX(), sprite.getY(), radius * 2);
     }
 
     @Override
     public void update() {
-        // TODO
+
+    }
+
+    /**
+     * Helper method to determine if the trigger is inside or outside of the BubbleSprite
+     *
+     * @param x        the X coordinate of the trigger
+     * @param y        the Y coordinate of the trigger
+     * @param distance the maximum distance allowed between the X,Y of the trigger and the X,Y of
+     *                 the BubbleSprite
+     * @return true if there is an overlap, false if not
+     */
+    protected boolean isOverlap(float x, float y, float distance) {
+        double spread = Math.sqrt((this.x - x) * (this.x - x) +
+                ((this.y - y) * (this.y - y)));
+        return spread <= distance;
     }
 }
--- app/src/main/java/ca/chrisbarrett/bubblecount/model/Sprite.java
+++ app/src/main/java/ca/chrisbarrett/bubblecount/model/Sprite.java
@@ -1,25 +1,103 @@
 package ca.chrisbarrett.bubblecount.model;
 
 /**
- * Created by chrisbarrett on 2016-06-29.
+ * All drawable sprites must implement
+ *
+ * @author Chris Barrett
+ * @since Jun 29, 2016
  */
 public interface Sprite {
 
+    /**
+     * Gets the X coordinate of the Sprite
+     *
+     * @return the X coordinate
+     */
     float getX();
 
+    /**
+     * Sets the X coordinate of the Sprite
+     *
+     * @param x the X coordinate value
+     */
     void setX(float x);
 
+    /**
+     * Gets the Y coordinate of the Sprite
+     *
+     * @return the Y coordinate
+     */
     float getY();
 
+    /**
+     * Sets the Y coordinate of the Sprite
+     *
+     * @param y the Y coordinate value
+     */
     void setY(float y);
 
+    /**
+     * Gets the radius of the Sprite
+     *
+     * @return radius
+     */
     float getRadius();
 
+    /**
+     * Sets the radius of the Sprite
+     *
+     * @param radius of the Sprite
+     */
     void setRadius(float radius);
 
+    /**
+     * Gets the text of the Sprite
+     *
+     * @return String object containing text. This object can be null.
+     */
+    String getText();
+
+    /**
+     * Sets the text of the Sprite
+     *
+     * @param text of the Sprite
+     */
+    void setText(String text);
+
+    /**
+     * Checks if the Sprite is visible
+     *
+     * @return true if the Sprite is visible, false if not visible
+     */
     boolean isVisible();
 
+    /**
+     * Sets the visible state of the Sprite
+     *
+     * @param isVisible is set to true if visible, false if not visible
+     */
     void setVisible(boolean isVisible);
 
+    /**
+     * Checks to see if the Sprite has collided with another Sprite object
+     *
+     * @param sprite Sprite object to be checked against
+     * @return true if the Sprites have collided
+     */
+    boolean isCollision(Sprite sprite);
+
+    /**
+     * Checks to see if the user has touched the Sprite
+     *
+     * @param x the X coordinate of the touch
+     * @param y the Y coordinate of the touch
+     * @return true if the Sprite has been touched, false if it has not been touched
+     */
+    boolean isTouched(float x, float y);
+
+    /**
+     * Updates the Sprite, such as with motion. Usually called prior to a draw event.
+     */
     void update();
+
 }
