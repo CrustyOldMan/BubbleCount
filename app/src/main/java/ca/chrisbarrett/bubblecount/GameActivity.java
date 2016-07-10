3dfd3566ea1b9b2fd9687ba14ca832225dc004a5
Chris Barrett <chris@mrenterprises.ca>
2016-07-09 23:50:43 -0700

    Implemented GameViewListener with test-run implementation of callbacks

--- app/src/main/java/ca/chrisbarrett/bubblecount/GameActivity.java
+++ app/src/main/java/ca/chrisbarrett/bubblecount/GameActivity.java
@@ -1,6 +1,9 @@
 package ca.chrisbarrett.bubblecount;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import ca.chrisbarrett.bubblecount.view.GameView;
@@ -12,25 +15,108 @@ import ca.chrisbarrett.bubblecount.view.GameView;
  * @see android.support.v7.app.AppCompatActivity
  * @since Jun 26, 2016
  */
-public class GameActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity implements GameView.OnGameViewListener {
 
     private GameView gameView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         gameView = new GameView(this);
         setContentView(gameView);
     }
 
     @Override
-    protected void onPause() {
+    public void onNewLevel (int level) {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this)
+                .setMessage("Level: " + level);
+        final AlertDialog alert = dialog.create();
+        alert.show();
+
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run () {
+                if (alert.isShowing()) {
+                    alert.dismiss();
+                }
+            }
+        };
+
+        alert.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss (DialogInterface dialog) {
+                handler.removeCallbacks(runnable);
+                gameView.onStart();
+            }
+        });
+        handler.postDelayed(runnable, 10000);
+    }
+
+    @Override
+    public void onGameStart () {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this)
+                .setMessage("Ready? Go!");
+        final AlertDialog alert = dialog.create();
+        alert.show();
+
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run () {
+                if (alert.isShowing()) {
+                    alert.dismiss();
+                }
+            }
+        };
+
+        alert.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss (DialogInterface dialog) {
+                handler.removeCallbacks(runnable);
+                gameView.onStart();
+            }
+        });
+
+        handler.postDelayed(runnable, 10000);
+    }
+
+    @Override
+    public void onGameEnd (long time) {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this)
+                .setMessage("Total time: " + time);
+        final AlertDialog alert = dialog.create();
+        alert.show();
+
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run () {
+                if (alert.isShowing()) {
+                    alert.dismiss();
+                }
+            }
+        };
+
+        alert.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss (DialogInterface dialog) {
+                handler.removeCallbacks(runnable);
+                gameView.onStart();
+            }
+        });
+
+        handler.postDelayed(runnable, 10000);
+    }
+
+    @Override
+    protected void onPause () {
         super.onPause();
         gameView.onPause();
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume () {
         super.onResume();
         gameView.onResume();
     }
