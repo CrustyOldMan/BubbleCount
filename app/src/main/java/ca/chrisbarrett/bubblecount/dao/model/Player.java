a3a1f77a931bfac8ab60ef1cd7e9f25d7c7e1ade
Chris Barrett <chris@mrenterprises.ca>
2016-07-13 22:52:04 -0700

    Added syncedOn field for future RESTful implementation

--- app/src/main/java/ca/chrisbarrett/bubblecount/dao/model/Player.java
+++ app/src/main/java/ca/chrisbarrett/bubblecount/dao/model/Player.java
@@ -18,30 +18,20 @@ import ca.chrisbarrett.bubblecount.util.Values;
  * @since Jul 10, 2016
  */
 
-public class Player implements Parcelable {
+public class Player  implements Parcelable {
 
-    @SuppressWarnings("unused")
-    public static final Parcelable.Creator<Player> CREATOR = new Parcelable.Creator<Player>() {
-        @Override
-        public Player createFromParcel (Parcel in) {
-            return new Player(in);
-        }
-
-        @Override
-        public Player[] newArray (int size) {
-            return new Player[size];
-        }
-    };
     private int id;                 // pk of the player
     private String name;            // name of the player
     private int yearOfBirth;        // year of birth of the player
     private Date createdOn;         // date the Player was created
+    private Date syncedOn;         // date the Player was created
+
 
     /**
      * Default constructor. Date will be null.
      */
     public Player () {
-        this(Values.NO_ID, "", Values.NO_ID, null);
+        this(Values.NO_ID, null, Values.NO_ID, null, null);
     }
 
     /**
@@ -51,7 +41,7 @@ public class Player implements Parcelable {
      * @param yearOfBirth
      */
     public Player (String name, int yearOfBirth) {
-        this(Values.NO_ID, name, yearOfBirth, Calendar.getInstance().getTime());
+        this(Values.NO_ID, name, yearOfBirth, Calendar.getInstance().getTime(), null);
     }
 
     /**
@@ -61,21 +51,17 @@ public class Player implements Parcelable {
      * @param name
      * @param yearOfBirth
      * @param createdOn
+     * @param syncedOn
      */
-    public Player (int id, String name, int yearOfBirth, Date createdOn) {
+    public Player (int id, String name, int yearOfBirth, Date createdOn, Date syncedOn) {
         this.id = id;
         this.name = name;
         this.yearOfBirth = yearOfBirth;
         this.createdOn = createdOn;
+        this.syncedOn = syncedOn;
     }
 
-    protected Player (Parcel in) {
-        id = in.readInt();
-        name = in.readString();
-        yearOfBirth = in.readInt();
-        long tmpCreatedOn = in.readLong();
-        createdOn = tmpCreatedOn != -1 ? new Date(tmpCreatedOn) : null;
-    }
+
 
     public int getId () {
         return id;
@@ -109,6 +95,14 @@ public class Player implements Parcelable {
         this.createdOn = createdOn;
     }
 
+    public Date getSyncedOn () {
+        return syncedOn;
+    }
+
+    public void setSyncedOn (Date syncedOn) {
+        this.syncedOn = syncedOn;
+    }
+
     @Override
     public boolean equals (Object o) {
         if (this == o) return true;
@@ -132,17 +126,41 @@ public class Player implements Parcelable {
         return result;
     }
 
+    protected Player(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+        yearOfBirth = in.readInt();
+        long tmpCreatedOn = in.readLong();
+        createdOn = tmpCreatedOn != -1 ? new Date(tmpCreatedOn) : null;
+        long tmpSyncedOn = in.readLong();
+        syncedOn = tmpSyncedOn != -1 ? new Date(tmpSyncedOn) : null;
+    }
+
     @Override
-    public int describeContents () {
+    public int describeContents() {
         return 0;
     }
 
     @Override
-    public void writeToParcel (Parcel dest, int flags) {
+    public void writeToParcel(Parcel dest, int flags) {
         dest.writeInt(id);
         dest.writeString(name);
         dest.writeInt(yearOfBirth);
         dest.writeLong(createdOn != null ? createdOn.getTime() : -1L);
+        dest.writeLong(syncedOn != null ? syncedOn.getTime() : -1L);
     }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Player> CREATOR = new Parcelable.Creator<Player>() {
+        @Override
+        public Player createFromParcel(Parcel in) {
+            return new Player(in);
+        }
+
+        @Override
+        public Player[] newArray(int size) {
+            return new Player[size];
+        }
+    };
 }
 
