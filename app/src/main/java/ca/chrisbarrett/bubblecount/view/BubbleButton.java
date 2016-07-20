71f47869daa33fa73ee9b9054782af41f5e85e29
Chris Barrett <chris@mrenterprises.ca>
2016-07-20 07:27:40 -0700

    "Bubble" views updated to support refactoring of colors.

--- app/src/main/java/ca/chrisbarrett/bubblecount/view/BubbleButton.java
+++ app/src/main/java/ca/chrisbarrett/bubblecount/view/BubbleButton.java
@@ -1,11 +1,9 @@
 package ca.chrisbarrett.bubblecount.view;
 
 import android.content.Context;
-import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.widget.Button;
 
-import ca.chrisbarrett.bubblecount.R;
 import ca.chrisbarrett.bubblecount.util.FontCache;
 
 /**
@@ -22,8 +20,6 @@ public class BubbleButton extends Button {
 
     public BubbleButton(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setBackgroundColor(ContextCompat.getColor(context, R.color.buttonBackground));
-        setTextColor(ContextCompat.getColor(context, R.color.colorPrimary));
         setTypeface(FontCache.getFont(context));
     }
 }
